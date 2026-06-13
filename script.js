0730c0a9e38b1b1ac1bebe39187de199321565db
Ramón González <gonzalez.ramon0211@gmail.com>
2026-06-13 16:29:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
         fecha: new Date().toLocaleString()
     };
 
-    const url = 'https://script.google.com/macros/s/AKfycbxMId5OWhBL4jvEbEqYkwCRDF5liSPGOituCK5HqdvJWRLrZj5iSe6UQctTpmxm-JTH/exec';
+    const url = 'https://script.google.com/macros/s/AKfycbwXCnW0n-Ap8gBVJlgKKCTiRfRQ0y5tXCC5ZnH1eIt6_VTeVV5xGvW-UbXMN33ejXU_/exec';
 
     fetch(url, {
         method: 'POST',
